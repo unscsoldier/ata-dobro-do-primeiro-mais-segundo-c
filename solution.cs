b893624c0ad31ce560d78193c8926e880ffb77f5
unscsoldier <lucas.azvdo@hotmail.com>
2026-08-14 21:33:12 -0300

    feat: publish Dobro do primeiro mais segundo (C#)

---
+++ solution.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int A = int.Parse(Console.ReadLine());
+        int B = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(
+          (2*A+B)
+        );
+    }
+}
